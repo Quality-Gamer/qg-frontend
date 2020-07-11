4b1a7d82791d60b5f9316e5d93c3d877ea1c6ee9
Agualuza <iagoagualuza@id.uff.br>
2020-07-11 19:00:52 -0300

    Update index.blade.php

--- resources/views/manager/index.blade.php
+++ resources/views/manager/index.blade.php
@@ -422,6 +422,7 @@
         }).done( r => {
             var idm = "#money-project";
             var idc = "#clock-project";
+            console.log(r);
             var res = r.response[0];
             var money = res.money;
             var time = res.time;
