0a1e305868c2f07ae8ff0f32bf6b532803ff5856
Agualuza <iagoagualuza@id.uff.br>
2020-07-12 14:15:49 -0300

    Update index.blade.php

--- resources/views/tests/index.blade.php
+++ resources/views/tests/index.blade.php
@@ -34,7 +34,7 @@
                             <div class="row">
                                 <div class="col-3"><img src='../assets/img/badges{{$a->badge}}'></div>
                                 <div class="col-4">{{$a->title}}</div>
-                                <div class="col-3"><img src='../assets/img/icons/coin_green.png'> {{$a->test_value}}</div>
+                                <div class="col-3"><img class="badge-size" src='../assets/img/icons/coin_green.png'> {{$a->test_value}}</div>
                                 <div class="col-2"><button class="btn btn-success">Fazer</button></div>
                             </div>
                         @endforeach
@@ -46,7 +46,7 @@
                             <div class="row">
                                 <div class="col-3"><img src='../assets/img/badges{{$d->badge}}'></div>
                                 <div class="col-4">{{$d->title}}</div>
-                                <div class="col-3"><img src='../assets/img/icons/coin_green.png'> {{$d->test_value}}</div>
+                                <div class="col-3"><img class="badge-size" src='../assets/img/icons/coin_green.png'> {{$d->test_value}}</div>
                                 <div class="col-2"><div class="badge badge-danger">Feito X</div></div>
                             </div>
                         @endforeach
