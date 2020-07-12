d364786828666f37db79330191cafb2cb20a0584
Agualuza <iagoagualuza@id.uff.br>
2020-07-12 13:59:47 -0300

    Alteracos no frontend na tela de testes

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -9,13 +9,34 @@ use Illuminate\Support\Facades\Auth;
 class TestsController extends Controller
 {
     public function index(Request $request){
+        $data = array();
         if($request->session()->has('user')){
             $user = new \App\User;
             $user->id = $request->session()->get('user')['id'];
             $user->name = $request->session()->get('user')['name'];
             $user->email = $request->session()->get('user')['email'];
             Auth::login($user);
-            $data = array();
+
+            $key = config('microsservices.key');
+            $url = config('microsservices.gateway');
+            $params = array(
+                "ms" => "tests",
+                "action" => "tests",
+                "params" => array(
+                    "user_id" => $user->id,
+                ),
+                "method" => "GET",
+                "cacheable" => 0,
+            );
+            
+            $response = APIService::postHttpRequest($url,$params,$key);
+            $body = $response["body"]->response;
+            if($body && $body->status == "OK"){
+                $data['allow'] = $body->response->allow;
+                $data['deny'] = $body->response->deny;
+            } else {
+                $data["message"] = "Erro ao conectar com o servidor.";
+            }
             return view("tests.index",$data);
         }
         return redirect("/");
--- resources/views/tests/index.blade.php
+++ resources/views/tests/index.blade.php
@@ -7,29 +7,72 @@
     </div>
     <div align="center"><h1 class="title-page">Conquistas</h1></div>
     <div class="jumbotron p-4 card-default col-lg-12 col-md-12 col-sm-12 bg-lg-green">
+    <div id="btn-close" class="btn-close pos-close"><a onclick="closeTab()">X</a></div>
+    <div id='badges-div'>    
         <div align="center" class="row">
-            <div class="col-2">
-                <img class='badge-size' src='../assets/img/badges/php.png'/>
+                <div class="col-2">
+                    <img class='badge-size' src='../assets/img/badges/php.png'/>
+                </div>
+                <div class="col-2">
+                    <img class='badge-size' src='../assets/img/badges/python.png'/>
+                </div>
+                <div class="col-2">
+                    <img class='badge-size' src='../assets/img/badges/go.png'/>
+                </div>
             </div>
-            <div class="col-2">
-                <img class='badge-size' src='../assets/img/badges/python.png'/>
+            <div style='position:absolute;bottom:0;right:0;margin-right:15px;' align="right">
+                <button onclick='LoadTests()' class="btn btn-success btn-icon btn-round">
+                    <i class="now-ui-icons ui-1_simple-add"></i>
+                </button>
             </div>
-            <div class="col-2">
-                <img class='badge-size' src='../assets/img/badges/go.png'/>
-            </div>
-        </div>
-        <div style='position:absolute;bottom:0;right:0;margin-right:15px;' align="right">
-            <button onclick='LoadTests()' class="btn btn-success btn-icon btn-round">
-                <i class="now-ui-icons ui-1_simple-add"></i>
-            </button>
-        </div>
+    </div>
+    <div class="d-none" id="tests-div">
+            <div align="center"><h2 class="title-card">Descrição</h2></div>
+                <div>
+                    @if ($allow)
+                        @foreach ($allow as $a)
+                            <div class="row">
+                                <div class="col-3"><img src='../assets/img/badges{{$a->badge}}'></div>
+                                <div class="col-4">{{$a->title}}</div>
+                                <div class="col-3"><img src='../assets/img/icons/coin_green.png'> {{$a->test_value}}</div>
+                                <div class="col-2"><button class="btn btn-success">Fazer</button></div>
+                            </div>
+                        @endforeach
+                    @endif
+                </div>
+                <div>
+                    @if ($deny)
+                        @foreach ($deny as $d)
+                            <div class="row">
+                                <div class="col-3"><img src='../assets/img/badges{{$d->badge}}'></div>
+                                <div class="col-4">{{$d->title}}</div>
+                                <div class="col-3"><img src='../assets/img/icons/coin_green.png'> {{$d->test_value}}</div>
+                                <div class="col-2"><div class="badge badge-danger">Feito X</div></div>
+                            </div>
+                        @endforeach
+                    @endif
+                </div>
+    </div>
     </div>
 </div>
 @endsection
 
 @section('scripts')
 <script>
+    $(document).ready(() => {
+        $("#btn-close").hide();
+        $("#tests-div").hide();
+    });
+
     const LoadTests = () => {
-        console.log("loadtests");
+        $("#btn-close").show();
+        $("#tests-div").show();
+        $("#badges-div").hide();
     }
+
+    const closeTab = () => {
+        $("#badges-div").show();
+        $("#btn-close").hide();
+        $("#tests-div").hide();
+    };
 </script>
