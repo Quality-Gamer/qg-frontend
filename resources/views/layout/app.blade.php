89abcc0a4026ca4b9368ae0e377a7e13270d1251
Agualuza <iagoagualuza@id.uff.br>
2020-07-11 22:43:27 -0300

    Update app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -23,7 +23,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/home.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>{{ config('app.name', 'Laravel') }}</title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
@@ -81,17 +81,17 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/profile">
+                            <a class="nav-link" href="/store">
                             <p>Loja</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/profile">
+                            <a class="nav-link" href="/tests">
                             <p>Conquistas</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/profile">
+                            <a class="nav-link" href="/ranking">
                             <p>Ranking</p>
                             </a>
                         </li>
