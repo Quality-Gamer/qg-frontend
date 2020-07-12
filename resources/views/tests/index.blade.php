c1b41bc2a71d7025ab7c882d4b519a42c9b47274
Agualuza <iagoagualuza@id.uff.br>
2020-07-11 22:43:21 -0300

    Create index.blade.php

---
+++ resources/views/tests/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.app')
+
+@section('content')
+<div class="col-lg-8 col-md-12 col-sm-12 col-12">
+    <div id="messages">
+        
+    </div>
+    <div align="center"><h1 class="title-page">Conquistas</h1></div>
+    <div class="jumbotron p-4 card-default col-lg-12 col-md-12 col-sm-12 bg-lg-green">
+        <div align="center" id="content-game">
+                
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
