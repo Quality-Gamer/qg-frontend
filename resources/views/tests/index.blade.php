1e2c7d5b5daec4a1ee0885ccd2665d6aa72822e1
Agualuza <iagoagualuza@id.uff.br>
2020-07-11 23:00:39 -0300

    Update index.blade.php

--- resources/views/tests/index.blade.php
+++ resources/views/tests/index.blade.php
@@ -7,8 +7,16 @@
     </div>
     <div align="center"><h1 class="title-page">Conquistas</h1></div>
     <div class="jumbotron p-4 card-default col-lg-12 col-md-12 col-sm-12 bg-lg-green">
-        <div align="center" id="content-game">
-                
+        <div align="center" class="row">
+            <div class="col-2">
+                <img src='../assets/img/badges/php.png'/>
+            </div>
+            <div class="col-2">
+                <img src='../assets/img/badges/python.png'/>
+            </div>
+            <div class="col-2">
+                <img src='../assets/img/badges/go.png'/>
+            </div>
         </div>
     </div>
 </div>
