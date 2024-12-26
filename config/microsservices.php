282e9adf47230010144db55f9e5566fc86698e93
Agualuza <iagoagualuza@msn.com>
2024-12-26 17:08:38 -0300

    Update microsservices.php

--- config/microsservices.php
+++ config/microsservices.php
@@ -3,9 +3,9 @@
    
 
 return [
-    'usuario' => 'https://qg-usuario.herokuapp.com/',
-    'manager' => env('MS_MANAGER'),
-    'gateway' => 'https://qg-api-gateway.herokuapp.com/gateway/route',
+    'usuario' => 'https://qg-usuario-docker-env.onrender.com',
+    'manager' => 'https://qg-manager.onrender.com',
+    'gateway' => 'https://api-gateway-8xar.onrender.com',
     'key' => 'Qg2cfd648bde02d8c3271ae1d9f7226f7eaZ',
 ]
 
